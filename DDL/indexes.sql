a2df09d6388988bd5ba484095a7b26ba0c2c66c4
Sxheeno <148947341+Sxheeno@users.noreply.github.com>
2025-05-15 14:09:24 +0300

    Update indexes.sql

--- DDL/indexes.sql
+++ DDL/indexes.sql
@@ -1,7 +1,6 @@
 CREATE INDEX idx_car_model ON car(model_id);
 CREATE INDEX idx_car_location ON car(location_id, status);
 CREATE INDEX idx_rental_dates ON rental(start_date, end_date);
-CREATE INDEX idx_customer_email ON customer(email);
 CREATE INDEX idx_car_status ON car(status);
 CREATE INDEX idx_model_manufacturer ON model(manufacturer_id, category_code);
 CREATE INDEX idx_rental_customer ON rental(customer_id, status);
